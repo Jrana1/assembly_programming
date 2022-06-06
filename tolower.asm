96d64ff403b6b32da8c9296c04b52df6f1d73ee4
Jrana1 <jewelbutex12@gmail.com>
2022-06-06 10:03:48 +0200

    tolower func updated

--- tolower.asm
+++ tolower.asm
@@ -1,13 +1,12 @@
 	.data
-	     str1:.asciiz "abc"
-	    
+	     str1:.asciiz "ABC"   
 	.text
 	   lui $a0,0x1001
 	   jal toLower
 end_of_programm:
            beq $0,$0,end_of_programm
 toLower: 
-       addiu $10,$zero,0x40  # from A
+       addiu $17,$zero,0x40  # from A
        addiu $13,$zero,0x5b  # to Z
        addiu $10,$a0,0       #taking the address of str1
        
@@ -15,13 +14,13 @@ toLower:
            lbu $12,0($10)
            beq $12,$0,return
            addiu $10,$10,1
-           slt $15,$12,$13 # set r15=1 if r12 <r13
-           slt $16,$10,$12 #set r16=1 if r10<12
+           slt $15,$17,$12 # set r15=1 if r12 <r13
+           slt $16,$12,$13 #set r16=1 if r10<12
            bne $15,$16,loop
-           addiu $12,$12,-32
+           addiu $12,$12,32
            addi $11,$10,-1
            sb $12,0($11)
            j loop
            
        return:
-           jr $ra
+           jr $ra   
