88e84c8d62ac0bb3a11d4106098885c93d8017d6
Jrana1 <jewelbutex12@gmail.com>
2022-06-06 13:49:34 +0200

    tolower using bit manipulation adde

--- tolower.asm
+++ tolower.asm
@@ -1,5 +1,5 @@
 	.data
-	     str1:.asciiz "ABC"   
+	     str1:.asciiz "aCb"   
 	.text
 	   lui $a0,0x1001
 	   jal toLower
@@ -9,6 +9,8 @@ toLower:
        addiu $17,$zero,0x40  # from A
        addiu $13,$zero,0x5b  # to Z
        addiu $10,$a0,0       #taking the address of str1
+       addiu $20,$zero,1     #init r20 with 1
+       sll $20,$20,5         #shifting r20 five times left to build 
        
        loop:       
            lbu $12,0($10)
@@ -17,10 +19,19 @@ toLower:
            slt $15,$17,$12 # set r15=1 if r12 <r13
            slt $16,$12,$13 #set r16=1 if r10<12
            bne $15,$16,loop
-           addiu $12,$12,32
+           #addiu $12,$12,32
+           or $12,$12,$20 #using bit masking
            addi $11,$10,-1
            sb $12,0($11)
            j loop
            
        return:
-           jr $ra   
+           jr $ra       
+       
+	  
+          
+        
+      
+      
+      	  
+
